af1f244ee8dae87fbf75cec70695eb2bff168f39
Ricardo Hernández Cambrón <riheca34@gmail.com>
2021-01-23 03:50:01 +0100

    Inicio practica concurrencia y red

---
+++ ConcurrenciaTopics/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ConcurrenciaTopics
+//
+//  Created by Ricardo Hernandez on 23/1/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var TableViewLatestTopic: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
