35eeb6133361cb1502ca21e1466980af8859d463
Ricardo Hernández Cambrón <riheca34@gmail.com>
2021-01-25 01:18:38 +0100

    practica tableviewcell

---
+++ ConcurrenciaTopics/TopCell.swift
@@ -0,0 +1,22 @@
+//
+//  Topic.swift
+//  ConcurrenciaTopics
+//
+//  Created by Ricardo Hernandez on 24/1/21.
+//
+
+import UIKit
+
+class TopicCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+}
